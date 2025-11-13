6d0571ea8622d21c6f4ba985bbe196e8849f54b3
Nilesh <akhilrana7500@gmail.com>
2025-11-13 22:01:56 +0530

    dsf

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:3000','http://localhost:3000/auth/signup'],
+  origin: [process.env.FRONTEND_URL || 'https://authx-delta.vercel.app/','https://authx-delta.vercel.app//auth/signup'],
   credentials: true,
 }));
 app.use(express.json());
