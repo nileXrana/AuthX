1adcba093da18d2fba44fdbd51cafbfa50f8a999
Nilesh <akhilrana7500@gmail.com>
2025-11-13 19:59:49 +0530

    Remove .next build cache from git tracking

--- frontEnd/.next/dev/static/chunks/app_page_tsx_6967d995._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_444ad9ce._.js"
-],
-    source: "dynamic"
-});
